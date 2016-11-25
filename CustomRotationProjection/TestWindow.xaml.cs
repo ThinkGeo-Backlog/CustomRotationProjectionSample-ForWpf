e19a83dc37be4da0fe756a225ee03eff915c8297
RexLiu <rexliu@thinkgeo.com>
2016-11-25 17:03:13 +0800

    Upload code.

---
+++ CustomRotationProjection/TestWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using ThinkGeo.MapSuite.Core;
+using ThinkGeo.MapSuite.WpfDesktopEdition;
+
+
+namespace CustomRotationProjection
+{
+    /// <summary>
+    /// Interaction logic for TestWindow.xaml
+    /// </summary>
+    public partial class TestWindow : Window
+    {
+        private Projections.CustomRotationProjection customRotationProjection;
+        
+        public TestWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Sets the correct map unit and the extent of the map.
+            wpfMap1.MapUnit = GeographyUnit.Meter;
+            wpfMap1.CurrentExtent = new RectangleShape(-10782920,3912077,-10779783,3910188);
+            wpfMap1.Background = new SolidColorBrush(Color.FromRgb(148, 196, 243));
+
+            GoogleMapsOverlay googleMapsOverlay = new GoogleMapsOverlay();
+            wpfMap1.Overlays.Add(googleMapsOverlay);
+
+            //Custom projection that will allow to project from State Plane Central North Texas to Spherical Mercator while applying a rotation.
+            customRotationProjection = new Projections.CustomRotationProjection(ManagedProj4Projection.GetEsriParametersString(102738),
+                ManagedProj4Projection.GetGoogleMapParametersString());
+            
+            ShapeFileFeatureLayer streetLayer = new ShapeFileFeatureLayer(@"../../data/Streets_subset.shp");
+            streetLayer.DrawingMarginPercentage = 100;
+            streetLayer.FeatureSource.Projection = customRotationProjection;
+            streetLayer.ZoomLevelSet.ZoomLevel01.DefaultLineStyle = LineStyles.CreateSimpleLineStyle
+                (GeoColor.StandardColors.Red, 3, GeoColor.FromArgb(255, GeoColor.StandardColors.Black), 5, true);
+            streetLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
+
+            LayerOverlay layerOverlay = new LayerOverlay();
+            layerOverlay.Layers.Add("StreetLayer", streetLayer);
+            wpfMap1.Overlays.Add("StreetOverlay", layerOverlay);
+
+            //Sets the pivot point to be the center of the layer.
+            streetLayer.Open();
+            customRotationProjection.PivotVertex = new Vertex(streetLayer.GetBoundingBox().GetCenterPoint());
+            streetLayer.Close();
+
+            customRotationProjection.Angle = 20;
+          
+            wpfMap1.Refresh();
+        }
+
+        private void btnRotateClockWise_Click(object sender, RoutedEventArgs e)
+        {
+            LayerOverlay streetLayerOverlay = (LayerOverlay)wpfMap1.Overlays["StreetOverlay"];
+            ShapeFileFeatureLayer streetLayer = (ShapeFileFeatureLayer)streetLayerOverlay.Layers["StreetLayer"];
+
+            //Set the Angle property of RotationProjection. Substract 5 to go clockwise.
+            streetLayer.Open();
+            customRotationProjection.Angle = customRotationProjection.Angle - 5;
+            streetLayer.Close();
+
+            wpfMap1.Refresh(streetLayerOverlay);
+        }
+
+        private void btnRotateCounterClockWise_Click(object sender, RoutedEventArgs e)
+        {
+            LayerOverlay streetLayerOverlay = (LayerOverlay)wpfMap1.Overlays["StreetOverlay"];
+            ShapeFileFeatureLayer streetLayer = (ShapeFileFeatureLayer)streetLayerOverlay.Layers["StreetLayer"];
+
+            //Set the Angle property of RotationProjection. Adds 5 to go clockwise.
+            streetLayer.Open();
+            customRotationProjection.Angle = customRotationProjection.Angle + 5;
+            streetLayer.Close();
+
+            wpfMap1.Refresh(streetLayerOverlay);
+        }
+
+       
+        private void wpfMap1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //Gets the PointShape in world coordinates from screen coordinates.
+            Point point = e.MouseDevice.GetPosition(null);
+            
+            ScreenPointF screenPointF = new ScreenPointF((float)point.X, (float)point.Y);
+            PointShape pointShape = ExtentHelper.ToWorldCoordinate(wpfMap1.CurrentExtent, screenPointF, (float)wpfMap1.Width, (float)wpfMap1.Height);
+
+            textBox1.Text = "X: " + Math.Round(pointShape.X) +
+                          "  Y: " + Math.Round(pointShape.Y);
+
+           }
+      }
+}
---
+++ README.md
@@ -0,0 +1,38 @@
+# CustomRotationProjectionSample-ForWpf
+
+### Description
+
+                In today’s project, we show how to create your own projection class that allows projecting a layer from any internal projection to any external while doing a rotation at the same time. Having the capability to apply those two operations in one step may come handy if the original layer was created in a different projection with the north at an angle and you want to align it on the base map. This is a Wpf project and you will need references to MapSuiteCore.dll and WpfDesktopEdition.dll to run it.
+              
+
+![Screenshot](https://github.com/ThinkGeo/CustomRotationProjectionSample-ForWpf/blob/master/ScreenShot.png)
+
+### Requirements
+This sample makes use of the following NuGet Packages
+
+[MapSuite 10.0.0.0](http:mapsuite.nuget)
+
+### About the Code
+
+Working...
+
+### Getting Help
+
+[Map Suite Wpf Wiki Resources](http://wiki.thinkgeo.com/wiki/map_suite_wpf_desktop_edition)
+
+[Map Suite Wpf Product Description](http://thinkgeo.com/map-suite-developer-gis/wpf-edition/)
+
+[ThinkGeo Community Site](http://community.thinkgeo.com/)
+
+[ThinkGeo Web Site](http://www.thinkgeo.com)
+
+### Key APIs
+This example makes use of the following APIs:
+
+Working...
+
+### About Map Suite
+Map Suite is a set of powerful development components and services for the .Net Framework.
+
+### About ThinkGeo
+ThinkGeo is a GIS (Geographic Information Systems) company founded in 2004 and located in Frisco, TX. Our clients are in more than 40 industries including agriculture, energy, transportation, government, engineering, software development, and defense.
